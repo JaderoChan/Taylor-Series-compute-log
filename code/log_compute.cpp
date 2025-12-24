e4171a49d60158ffb9291c60d0c0793331125866
頔珞JaderoChan <c_dl_cn@outlook.com>
2025-12-24 10:09:44 +0800

    - 改用自己实现的pow函数 - 将ln(2)、ln(3)作为字面量 - 一些其他的小改动

--- code/log_compute.cpp
+++ code/log_compute.cpp
@@ -5,87 +5,52 @@
 namespace my_log
 {
 
-/*
-$$
-\because ln(2^\frac{1}{16})=\frac{1}{16}ln(2)
-\\
-\therefore ln(2)=16ln(2^\frac{1}{16})
-\\
-\ast \quad 2-1=1, \ \sqrt[16]{2}-1\approx 0.0442737 \quad \ast
-$$
-*/
-/// @brief Compute the ln(2) by Taylor series.
-/// @param series The series larger, result more precise, but no obvious effects if the series over 7 places.
-double ln2ByTaylorSeries(int series = 1e6) {
-    double sum = 0.0;
-    double x = std::pow(2.0, 0.0625) - 1.0;
-    bool isPositive = true;
-    for (int i = 1; i <= series; ++i) {
-        if (isPositive)
-            sum += std::pow(x, i) / i;
-        else
-            sum -= std::pow(x, i) / i;
-        isPositive = !isPositive;
+template <typename T>
+constexpr double powInt(T base, int exp) {
+    if (exp == 0)   return 1.0;
+    if (exp < 0)    return 1.0 / powInt(base, -exp);
+
+    double result = 1.0;
+    while (exp) {
+        if (exp & 1)
+            result *= base;
+        base *= base;
+        exp >>= 1;
     }
-    return 16.0 * sum;
+
+    return result;
 }
 
-/*
-$$
-\because ln(3^\frac{1}{16})=\frac{1}{16}ln(3)
-\\
-\therefore ln(3)=16ln(3^\frac{1}{16})
-\\
-\ast \quad 3-1=2, \ \sqrt[16]{3}-1\approx 0.0710754 \quad \ast
-$$
-*/
-/// @brief Compute the ln(2) by Taylor series.
-/// @param series The series larger, result more precise, but no obvious effects if the series over 7 places.
-double ln3ByTaylorSeries(int series = 1e6) {
-    double sum = 0.0;
-    double x = std::pow(3.0, 0.0625) - 1.0;
-    bool isPositive = true;
-    for (int i = 1; i <= series; ++i) {
-        if (isPositive)
-            sum += std::pow(x, i) / i;
-        else
-            sum -= std::pow(x, i) / i;
-        isPositive = !isPositive;
-    }
-    return 16.0 * sum;
-}
-
-const double LN2 = ln2ByTaylorSeries();
-const double LN3 = ln3ByTaylorSeries();
+constexpr double LN2 = 0.693147180559945309417232121458;
+constexpr double LN3 = 1.098612288668109691395245236922;
+constexpr double SQRT2 = 1.414213562373095048801688724209;
 
 /// @param series The series larger, result more precise.
 /// @note The m in range[1,2), 2m/3 in range[2/3, 4/3). Let "x" moer close 1 for higher precision.
 double lnVersion1(double x, int series = 10) {
-    if (x <= 0.0)      // The log() domain of definition is (0, +infty).
+    if (x <= 0.0)   // The log() domain of definition is (0, +infty).
         throw std::domain_error("The x must is positive.");
-    if (x == 1.0)    // The ln(1) = 0.
+    if (x == 1.0)   // The ln(1) = 0.
         return 0.0;
     // Cast the double to unsigned long long for bit operation.
-    unsigned long long _x = *reinterpret_cast<unsigned long long*>(&x);
+    unsigned long long v = *reinterpret_cast<unsigned long long*>(&x);
     // Get the float point num's exp.
-    int j = _x >> 52;
+    int j = v >> 52;
     // Get the float point num's mantissa. The (0x3ff0ULL << 48) is a double it = 1.0.
-    _x = 0x3ff0ULL << 48 ^ (_x << 12 >> 12);
+    v = 0x3ff0ULL << 48 ^ (v << 12 >> 12);
     // Cast the bit operated unsigned long long to double.
-    double m = *reinterpret_cast<double*>(&_x);
+    double m = *reinterpret_cast<double*>(&v);
     // Let "x" more close 1 for higher precision.
-    m = 2.0 * m / 3.0;
+    // m = 2.0 * m / 3.0;
+    m *= 0.666666666666666666666666666666;
     // The Taylor series expanded summation.
-    double sum = 0.0;
-    bool isPositive = true;
+    double result = 0.0;
+    int sign = 1;
     for (int i = 1; i <= series; ++i) {
-        if (isPositive)
-            sum += std::pow((m - 1.0), i) / i;
-        else
-            sum -= std::pow((m - 1.0), i) / i;
-        isPositive = !isPositive;
+        result += sign * powInt((m - 1.0), i) / i;
+        sign *= -1;
     }
-    return sum + LN3 - LN2 + (j - 1023.0) * LN2;
+    return result + LN3 - LN2 + (double) (j - 1023) * LN2;
 }
 
 /// @param series The series larger, result more precise
@@ -95,21 +60,18 @@ double lnVersion2(double x, int series = 10) {
         throw std::domain_error("The x must is positive.");
     if (x == 1.0)
         return 0.0;
-    unsigned long long _x = *reinterpret_cast<unsigned long long*>(&x);
-    int j = _x >> 52;
-    _x = 0x3ff0ULL << 48 ^ (_x << 12 >> 12);
-    double m = *reinterpret_cast<double*>(&_x);
-    m = std::pow(2.0, 0.5) * m / 2.0;
-    double sum = 0.0;
-    bool isPositive = true;
+    unsigned long long v = *reinterpret_cast<unsigned long long*>(&x);
+    int j = v >> 52;
+    v = 0x3ff0ULL << 48 ^ (v << 12 >> 12);
+    double m = *reinterpret_cast<double*>(&v);
+    m = SQRT2 * m / 2.0;
+    double result = 0.0;
+    int sign = 1;
     for (int i = 1; i <= series; ++i) {
-        if (isPositive)
-            sum -= std::pow((m - 1.0), i) / i;
-        else
-            sum += std::pow((m - 1.0), i) / i;
-        isPositive = !isPositive;
+        result += sign * powInt((m - 1.0), i) / i;
+        sign *= -1;
     }
-    return sum + LN2 / 2.0 + (j - 1023.0) * LN2;
+    return result + LN2 / 2.0 + (double) (j - 1023) * LN2;
 }
 
 double log(double base, double x) {
@@ -133,16 +95,16 @@ double log(double base, double x) {
 
 }
 
-namespace benchmark_test
+namespace benchmark
 {
 
-void logPrecisionCompare(int testCount = 64, double start = 1.0, double step = 0.5) {
+void lnPrecisionCompare(int testCount = 64, double start = 1.0, double step = 0.5) {
     std::cout.precision(12);
 
     for (int i = 0; i < testCount; ++i, start += step) {
-        double a1 = my_log::lnVersion1(start);
+        double a1 = my_log::lnVersion1(start, 1000);
         double a2 = std_log::ln(start);
-        
+
         std::cout << "X: " << start << ";   \t";
         std::cout << "My Log: " << a1 << ";   \t";
         std::cout << "STD Log: " << a2 << ";   \t";
@@ -150,14 +112,14 @@ void logPrecisionCompare(int testCount = 64, double start = 1.0, double step = 0
     }
 }
 
-void logPerformanceCompare(int testCount = 1e6, double start = 1.0, double step = 0.5) {
+void lnPerformanceCompare(int testCount = 1e6, double start = 1.0, double step = 0.5) {
     time_t startTime = clock();
     double x = start;
     for (int i = 0; i < testCount; ++i, x += step)
         std_log::ln(x);
     std::cout << "[STD][" << testCount << "]: ";
     std::cout << clock() - startTime << "msec" << '\n';
-    
+
     startTime = clock();
     x = start;
     for (int i = 0; i < testCount; ++i, x += step)
@@ -170,7 +132,7 @@ void logPerformanceCompare(int testCount = 1e6, double start = 1.0, double step 
 
 int main()
 {
-    benchmark_test::logPrecisionCompare();
-    benchmark_test::logPerformanceCompare(1e6);
+    benchmark::lnPrecisionCompare();
+    benchmark::lnPerformanceCompare(1e6);
     return 0;
 }
